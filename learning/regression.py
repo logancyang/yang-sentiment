bccfc9f0a8142402d4fc8639fb47c341ef4ae7b5
Logan Yang <logan1934@gmail.com>
2019-08-30 06:57:20 -0400

    Use delta for trendline significance

--- learning/regression.py
+++ learning/regression.py
@@ -4,7 +4,7 @@ from sklearn import linear_model
 
 
 # pylint: disable=logging-fstring-interpolation
-def sklearn_linear_regression(X, y):
+def linear_regression(X, y):
     """Perform basic OLS linear regression on 1D data and returns predicted y
     values as a list. Return the predicted values and whether the coefficient is positive
 
@@ -17,9 +17,9 @@ def sklearn_linear_regression(X, y):
     reg = linear_model.LinearRegression()
     reg.fit(X_reshaped, y)
     y_pred = reg.predict(X_reshaped)
-    return list(y_pred), bool(reg.coef_[0] > 0)
+    return list(y_pred), _get_signif_delta(first=y_pred[0], last=y_pred[-1])
 
-def linear_regression(t, y):
+def stats_linear_regression(t, y):
     """Perform basic OLS linear regression on 1D data and returns predicted y
     values as a list. Return the predicted values and the trend value,
     0 for insignificant, 1 for positive, -1 for negative
@@ -40,6 +40,13 @@ def _get_signif(coeff, pvalue):
     if coeff > 0:
         return 1
     return -1
+
+def _get_signif_delta(first, last, sensitivity=0.03):
+    if last > first * (1 + sensitivity):
+        return 1
+    elif last < first * (1 - sensitivity):
+        return -1
+    return 0
 
 """Normalization and standardization do not affect regression inference p values"""
 def _normalize(nparray):
