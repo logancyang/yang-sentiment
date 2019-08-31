977860f3813d883cfa7d8e1fe647a92118813cc6
Logan Yang <logan1934@gmail.com>
2019-08-31 12:30:29 -0400

    Debug ebextension apache config

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+PORT=7800
+DB_NAME=crypto_sentiment_db
+DB_USER=dukelolo_crypto
+DB_PASSWORD=h3v5H8R9
+RDS_POSTGRES_ENDPOINT=crypto-sentiment-db.cjnd7boyg5li.us-east-1.rds.amazonaws.com
+
 gunicorn --worker-class=gevent -w 4 -t 20000 application:app -b 0.0.0.0:$PORT
