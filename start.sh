e7a38d593fbb7b9f6b73ab762b05d42b30c9b6fc
Logan Yang <logan1934@gmail.com>
2019-08-31 12:52:13 -0400

    Add wsgi.py

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ DB_USER=dukelolo_crypto
 DB_PASSWORD=h3v5H8R9
 RDS_POSTGRES_ENDPOINT=crypto-sentiment-db.cjnd7boyg5li.us-east-1.rds.amazonaws.com
 
-gunicorn --worker-class=gevent -w 4 -t 20000 application:app -b 0.0.0.0:$PORT
+gunicorn --worker-class=gevent -w 4 -t 20000 wsgi:application -b 0.0.0.0:$PORT
