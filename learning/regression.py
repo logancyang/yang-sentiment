6ec4276b9a8589a5b93f002f827bc1da8764c76a
Logan Yang <logan1934@gmail.com>
2019-08-31 11:19:07 -0400

    Remove pandas and statsmodels

--- learning/regression.py
+++ learning/regression.py
@@ -1,5 +1,5 @@
 import numpy as np
-import statsmodels.api as sm
+# import statsmodels.api as sm
 from sklearn import linear_model
 
 
@@ -19,27 +19,27 @@ def linear_regression(X, y):
     y_pred = reg.predict(X_reshaped)
     return list(y_pred), _get_signif_delta(first=y_pred[0], last=y_pred[-1])
 
-def stats_linear_regression(t, y):
-    """Perform basic OLS linear regression on 1D data and returns predicted y
-    values as a list. Return the predicted values and the trend value,
-    0 for insignificant, 1 for positive, -1 for negative
+# def stats_linear_regression(t, y):
+#     """Perform basic OLS linear regression on 1D data and returns predicted y
+#     values as a list. Return the predicted values and the trend value,
+#     0 for insignificant, 1 for positive, -1 for negative
 
-    Arguments:
-        X {list} -- 1D list of x values
-        y {list} -- 1D list of y values
-    """
-    t, y = np.array(t), np.array(y)
-    t = sm.add_constant(t)
-    model = sm.OLS(y, t).fit()
-    y_pred = model.predict(t)
-    return list(y_pred), _get_signif(model.params[1], model.pvalues[1])
+#     Arguments:
+#         X {list} -- 1D list of x values
+#         y {list} -- 1D list of y values
+#     """
+#     t, y = np.array(t), np.array(y)
+#     t = sm.add_constant(t)
+#     model = sm.OLS(y, t).fit()
+#     y_pred = model.predict(t)
+#     return list(y_pred), _get_signif(model.params[1], model.pvalues[1])
 
-def _get_signif(coeff, pvalue):
-    if pvalue >= 0.1:
-        return 0
-    if coeff > 0:
-        return 1
-    return -1
+# def _get_signif(coeff, pvalue):
+#     if pvalue >= 0.1:
+#         return 0
+#     if coeff > 0:
+#         return 1
+#     return -1
 
 def _get_signif_delta(first, last, sensitivity=0.05):
     if last > first * (1 + sensitivity):
