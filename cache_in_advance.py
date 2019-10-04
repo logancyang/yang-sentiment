4767b0ae62cddf5cb2330b42cc00bd6d6937ff27
Logan Yang <logan1934@gmail.com>
2019-10-04 17:14:13 -0400

    Add scheduled job for advanced caching

--- cache_in_advance.py
+++ cache_in_advance.py
@@ -1,20 +1,20 @@
 import logging
 import time
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from application import _tweets_chart_request, top_retweets
+from apscheduler.schedulers.blocking import BlockingScheduler
+from application import _tweets_chart_request, top_retweets, wordcloud
 
 
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=10)
 def cache_in_advance():
-    while True:
-        _tweets_chart_request(chart_type='14d_at_1d')
-        _tweets_chart_request(chart_type='72hr_at_1hr')
-        _tweets_chart_request(chart_type='72h_for_loc')
-        top_retweets()
-        logging.info(f"Advance caching executed at {datetime.now()}")
-        time.sleep(10 * 60)
+    _tweets_chart_request(chart_type='14d_at_1d')
+    _tweets_chart_request(chart_type='72hr_at_1hr')
+    _tweets_chart_request(chart_type='72h_for_loc')
+    top_retweets()
+    wordcloud()
+    logging.info(f"Scheduled Job: advance caching executed at {datetime.now()}")
 
-def start_advance_caching():
-    sched = BackgroundScheduler(daemon=True)
-    sched.add_job(cache_in_advance, 'interval', minutes=10)
-    sched.start()
+
+sched.start()
