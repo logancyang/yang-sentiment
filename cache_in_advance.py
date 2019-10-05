0f89a9d05d3968c5190e2746794f6411009b03c8
Logan Yang <logan1934@gmail.com>
2019-10-05 10:05:26 -0400

    Update clock process to use new query funcs

--- cache_in_advance.py
+++ cache_in_advance.py
@@ -2,18 +2,18 @@ import logging
 import time
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-from application import _tweets_chart_request, top_retweets, wordcloud
+from query_funcs import tweets_chart_request, get_top_retweets, get_wordcloud
 
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=10)
 def cache_in_advance():
-    _tweets_chart_request(chart_type='14d_at_1d')
-    _tweets_chart_request(chart_type='72hr_at_1hr')
-    _tweets_chart_request(chart_type='72h_for_loc')
-    top_retweets()
-    wordcloud()
+    tweets_chart_request(chart_type='14d_at_1d')
+    tweets_chart_request(chart_type='72hr_at_1hr')
+    tweets_chart_request(chart_type='72h_for_loc')
+    get_top_retweets()
+    get_wordcloud()
     logging.info(f"Scheduled Job: advance caching executed at {datetime.now()}")
 
 
